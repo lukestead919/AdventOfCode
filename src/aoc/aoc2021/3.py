287405bd4101a5a0df63fc503dafa635a685c72c
lukestead <luke.stead@sonocent.com>
2021-12-03 13:39:15 +0000

    Little refactor

--- src/aoc/aoc2021/3.py
+++ src/aoc/aoc2021/3.py
@@ -16,13 +16,20 @@ test = ["00100",
         "01010"]
 
 
-def merge_binaries(lst: list[str]) -> str:
-    bin_length = len(lst[0])
-    lst_length = len(lst)
-    bitwise = [[int(lst[j][i]) for j in range(lst_length)] for i in range(bin_length)]
-    sums = [sum(a) for a in bitwise]
-    collapsed = ["1" if a >= lst_length / 2 else "0" for a in sums]
+def merge_binaries(binaries: list[str]) -> str:
+    bin_length = len(binaries[0])
+    collapsed = [merge_bits_at_index(binaries, i) for i in range(bin_length)]
     return ''.join(collapsed)
+
+
+def merge_bits_at_index(binaries: list[str], index: int) -> str:
+    bits = [binaries[i][index] for i in range(len(binaries))]
+    return merge_bits(bits)
+
+
+def merge_bits(bits: list[str]) -> str:
+    num_of_ones = sum(int(b) for b in bits)
+    return "1" if num_of_ones >= len(bits) / 2 else "0"
 
 
 def calculate_power(data: list[str]):
@@ -42,11 +49,11 @@ def flip_bit(bit: str) -> str:
     return "1" if bit == "0" else "0"
 
 
-def find_common(lst: list[str], most_common: bool) -> str:
+def find_rating(lst: list[str], most_common: bool) -> str:
     matches = lst[::]
     index = 0
     while len(matches) > 1:
-        to_match = merge_binaries(matches)[index]
+        to_match = merge_bits_at_index(matches, index)
         if not most_common:
             to_match = flip_bit(to_match)
         matches = [a for a in matches if a[index] == to_match]
@@ -56,14 +63,14 @@ def find_common(lst: list[str], most_common: bool) -> str:
 
 
 def calculate_life_support(data: list[str]):
-    o2 = find_common(data, True)
-    co2 = find_common(data, False)
+    o2 = find_rating(data, True)
+    co2 = find_rating(data, False)
     print(o2, co2)
     return int(o2, 2) * int(co2, 2)
 
 
-# print("part 1 test", calculate_power(test))
-# print("part 2 test", calculate_life_support(test))
+print("part 1 test", calculate_power(test))
+print("part 2 test", calculate_life_support(test))
 
 print("part 1", calculate_power(data))
 print("part 2", calculate_life_support(data))
