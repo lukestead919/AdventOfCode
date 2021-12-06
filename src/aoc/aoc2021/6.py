a7a90ae6a413de2d67523c55581bb9750ed2b818
lukestead <luke.stead@sonocent.com>
2021-12-06 10:33:44 +0000

    move

--- src/aoc/aoc2021/6.py
+++ src/aoc/aoc2021/6.py
@@ -16,16 +16,15 @@ class FishPopulation:
         return sum(self.pop)
 
 
-data = read_data_file_as_lines(6)[0]
-fish_timers = [int(s) for s in data.split(",")]
-
-
 def run_pop_for_days(fish_timers: list[int], days: int):
     fish_pop = FishPopulation(fish_timers)
     for _ in range(days):
         fish_pop.generation()
     return fish_pop.total_pop()
 
+
+data = read_data_file_as_lines(6)[0]
+fish_timers = [int(s) for s in data.split(",")]
 
 print("part 1", run_pop_for_days(fish_timers, 80))
 print("part 2", run_pop_for_days(fish_timers, 256))
