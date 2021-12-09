785b6de768330395d992f9e0220a2856923de007
lukestead <luke.stead@sonocent.com>
2021-12-09 16:55:01 +0000

    Colours!

--- src/aoc/aoc2021/9.py
+++ src/aoc/aoc2021/9.py
@@ -1,6 +1,41 @@
 from utils import read_data_file_as_lines, Point
 import numpy as np
 from collections import Counter
+import colorsys
+
+
+class colors: # You may need to change color settings
+    BLACK = "000000"
+    A = "1E90FF"
+    B = "187DE9"
+    C = "126AD2"
+    D = "0C56BC"
+    E = "0643A5"
+    F = "00308F"
+
+
+def getcolour(size: int):
+    if size > 5/6:
+        return colors.F
+    elif size > 4/6:
+        return colors.E
+    elif size > 3/6:
+        return colors.D
+    elif size > 2/6:
+        return colors.C
+    elif size > 1/6:
+        return colors.B
+    elif size > 0:
+        return colors.A
+    else:
+        return colors.BLACK
+
+
+def square(hex_string: str) -> str:
+    red = int(hex_string[:2], 16)
+    green = int(hex_string[2:4], 16)
+    blue = int(hex_string[4:6], 16)
+    return f"\033[48:2::{red}:{green}:{blue}m   \033[49m"
 
 
 class Heightmap:
@@ -21,7 +56,7 @@ class Heightmap:
             index = Point(i, j)
             value = self.array[index()]
             neighbours = self.get_neighbours_values(index)
-            if all([neighbour > value for neighbour in neighbours]):
+            if all(neighbour > value for neighbour in neighbours):
                 sum += value + 1
 
         return sum
@@ -52,6 +87,19 @@ class Heightmap:
         most_common_sizes = [b[1] for b in most_common]
         return np.product(most_common_sizes)
 
+    def print(self):
+        sizes = Counter(self.basinmap.flatten())
+        sizes[0] = 0
+        largest = sizes.most_common(1)[0][1]
+
+        def colour(basin_num: int):
+            ratio = sizes[basin_num]/largest
+            return square(getcolour(ratio))
+
+        colour_map = np.vectorize(colour)(self.basinmap)
+        for i in colour_map:
+            print(''.join(i))
+
 
 data = read_data_file_as_lines(9)
 datai = [[int(a) for a in line] for line in data]
@@ -60,3 +108,5 @@ heightmap = Heightmap(np.array(datai))
 heightmap.populate_basin_map()
 print("part 1", heightmap.count_minima())
 print("part 2", heightmap.calculate_basin_score())
+
+heightmap.print()
