0639a37376fd1c47c672ee1b073e8477fa757ab4
lukestead <luke.stead@sonocent.com>
2021-12-18 11:44:59 +0000

    18 - Split entries into classes

--- src/aoc/aoc2021/18.py
+++ src/aoc/aoc2021/18.py
@@ -2,20 +2,63 @@ from utils import read_data_file_as_lines
 from functools import reduce
 from itertools import product
 from copy import deepcopy
-# from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
+from typing import Optional
 
 data = read_data_file_as_lines(18)
 
 
-# class Entry:
-#     @abstractmethod
-#     def add_left(self, num: int):
-#
-# class Number(Entry):
+class Entry(ABC):
+    @abstractmethod
+    def add_right(self, num: int) -> None:
+        pass
+
+    @abstractmethod
+    def add_left(self, num: int) -> None:
+        pass
+
+    @abstractmethod
+    def magnitude(self) -> int:
+        pass
+
+    @abstractmethod
+    def explode(self, depth: int) -> Optional[tuple]:
+        pass
+
+    @abstractmethod
+    def split(self) -> Optional:
+        pass
 
 
-class Pair:
-    def __init__(self, first, second):
+class Number(Entry):
+    def __init__(self, value: int):
+        self.value = value
+
+    def __add__(self, other):
+        self.value += other.value
+
+    def __repr__(self):
+        return f"{self.value}"
+
+    def add_right(self, num: int):
+        self.value += num
+
+    def add_left(self, num: int):
+        self.value += num
+
+    def magnitude(self):
+        return self.value
+
+    def explode(self, depth: int):
+        pass
+
+    def split(self):
+        if self.value > 9:
+            return Pair(Number(self.value // 2), Number(self.value - (self.value // 2)))
+
+
+class Pair(Entry):
+    def __init__(self, first: Entry, second: Entry):
         self.first = deepcopy(first)
         self.second = deepcopy(second)
 
@@ -34,96 +77,59 @@ class Pair:
         return pair
 
     def magnitude(self):
-        if isinstance(self.first, Pair):
-            mag_left = self.first.magnitude()
-        else:
-            mag_left = self.first
-
-        if isinstance(self.second, Pair):
-            mag_right = self.second.magnitude()
-        else:
-            mag_right = self.second
-
-        return 3 * mag_left + 2 * mag_right
+        return 3 * self.first.magnitude() + 2 * self.second.magnitude()
 
     def reduce(self):
-        # original = self.deepcopy()
         while True:
             result = self.explode(0)
             if result:
                 continue
-            # print(self)
-            # if original != self:
-            #     original = self
-            #     continue
 
             result = self.split()
             if result:
                 continue
-            # if original != self:
-            #     original = self
-            #     continue
             break
 
     def explode(self, depth: int) -> tuple[int, int]:
         if depth == 4:
-            return self.first, self.second
+            return self.first.value, self.second.value
 
-        if isinstance(self.first, Pair):
-            first_explode = self.first.explode(depth + 1)
-            if first_explode is not None:
-                if depth == 3:
-                    self.first = 0
+        explode_result = self.first.explode(depth + 1)
+        if explode_result:
+            if depth == 3:
+                self.first = Number(0)
+            add_to_left, add_to_right = explode_result
 
-                if isinstance(self.second, int):
-                    self.second += first_explode[1]
-                else:
-                    self.second.add_left(first_explode[1])
+            self.second.add_left(add_to_right)
+            return explode_result[0], 0
 
-                return first_explode[0], 0
+        explode_result = self.second.explode(depth + 1)
+        if explode_result:
+            if depth == 3:
+                self.second = Number(0)
+            add_to_left, add_to_right = explode_result
 
-        if isinstance(self.second, Pair):
-            second_explode = self.second.explode(depth + 1)
-            if second_explode is not None:
-                if depth == 3:
-                    self.second = 0
-
-                # add_to_left = second_explode
-                if isinstance(self.first, int):
-                    self.first += second_explode[0]
-                else:
-                    self.first.add_right(second_explode[0])
-
-                return 0, second_explode[1]
+            self.first.add_right(add_to_left)
+            return 0, explode_result[1]
 
     def add_left(self, num: int):
-        if isinstance(self.first, int):
-            self.first += num
-        else:
-            self.first.add_left(num)
+        self.first.add_left(num)
 
     def add_right(self, num: int):
-        if isinstance(self.second, int):
-            self.second += num
-        else:
-            self.second.add_right(num)
+        self.second.add_right(num)
 
     def split(self):
-        if isinstance(self.first, int) and self.first > 9:
-            self.first = Pair(self.first // 2, self.first - (self.first // 2))
+        split_result = self.first.split()
+        if split_result:
+            if isinstance(split_result, Entry):
+                self.first = split_result
             return True
-        elif isinstance(self.first, Pair):
-            split_result = self.first.split()
-            if split_result is not None:
-                return split_result
 
-        if isinstance(self.second, int) and self.second > 9:
-            self.second = Pair(self.second // 2, self.second - (self.second // 2))
+        split_result = self.second.split()
+        if split_result:
+            if isinstance(split_result, Entry):
+                self.second = split_result
             return True
-        elif isinstance(self.second, Pair):
-            split_result = self.second.split()
-            if split_result is not None:
-                return split_result
 
 
 def parse(s: str):
@@ -138,10 +144,9 @@ def parse(s: str):
             elif char == "," and open_brackets == 0:
                 first = middle[0:idx]
                 second = middle[idx+1:]
-                # print(s, first, second)
                 return Pair(parse(first), parse(second))
     else:
-        return int(s)
+        return Number(int(s))
 
 
 def test():
@@ -155,7 +160,7 @@ def main():
     pairs = [parse(s) for s in data]
     result = reduce(lambda x, y: x+y, pairs)
     print("part 1", result.magnitude())
-    print("part 2", max(x.add_and_reduce(y).magnitude() for x, y in product(pairs, pairs)))
+    print("part 2", max((x+y).magnitude() for x, y in product(pairs, pairs)))
 
 
 # test()
