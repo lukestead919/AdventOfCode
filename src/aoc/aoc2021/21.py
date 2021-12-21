6d18f51f45db04e83b74f116f14de3877984b8ed
lukestead <luke.stead@sonocent.com>
2021-12-21 15:08:01 +0000

    21

--- src/aoc/aoc2021/21.py
+++ src/aoc/aoc2021/21.py
@@ -1,6 +1,13 @@
 from utils import read_data_file_as_lines
 from functools import cache, reduce
 from itertools import product
+from operator import add
+from time import time
+
+
+def to_space(score: int):
+    return ((score - 1) % 10) + 1
+
 
 class Game1:
     def __init__(self, player1: int, player2: int):
@@ -13,17 +20,15 @@ class Game1:
     def play(self):
         while self.player1_score < 1000 and self.player2_score < 1000:
             self.play_next()
-        # print(self.round, self.player1_score, self.player2_score)
         return self.round * min(self.player1_score, self.player2_score)
 
     def play_next(self):
-        score = sum([self.roll_dice() for _ in range(3)])
-        # print(score)
+        dice_roll = sum([self.roll_dice() for _ in range(3)])
         if self.round % 2 == 1:
-            self.player1 = ((self.player1 + score - 1) % 10) + 1
+            self.player1 = to_space(self.player1 + dice_roll)
             self.player1_score += self.player1
         else:
-            self.player2 = ((self.player2 + score - 1) % 10) + 1
+            self.player2 = to_space(self.player2 + dice_roll)
             self.player2_score += self.player2
 
     def roll_dice(self):
@@ -47,52 +52,32 @@ class Game2:
     def __init__(self, player1: int, player2: int):
         self.player1 = player1
         self.player2 = player2
-        self.player1_score = 0
-        self.player2_score = 0
-        self.round = 0
 
-    @cache
-    def play_from_start(self):
-        scores = self.play(self.player1, self.player2, 0, 0, True)
+    def play(self):
+        start_time = time()
+        scores = self.number_of_wins(self.player1, self.player2, 0, 0, True)
+        print(f"took {time() - start_time} s")
         return max(scores.player1, scores.player2)
 
     @cache
-    def play(self, player1: int, player2: int, player1_score: int, player2_score: int, player1_turn: bool) -> Scores:
-        print(player1, player2, player1_score, player2_score)
+    def number_of_wins(self, player1: int, player2: int, player1_score: int, player2_score: int,
+                       player1_turn: bool) -> Scores:
         if player1_score >= 21:
             return Scores(1, 0)
         elif player2_score >= 21:
             return Scores(0, 1)
 
-        scores = [sum(p) for p in product(range(1, 4), repeat=3)]
+        dice_rolls = [sum(p) for p in product(range(1, 4), repeat=3)]
         if player1_turn:
-            new_player1s = [((player1 + score - 1) % 10) + 1 for score in scores]
-            s = [self.play(new_player1, player2, player1_score + new_player1, player2_score, False) for new_player1 in
-                 new_player1s]
-            return reduce(lambda x, y: x+y, s)
+            new_player1s = [to_space(player1 + roll) for roll in dice_rolls]
+            results = [self.number_of_wins(new_player1, player2, player1_score + new_player1, player2_score, False)
+                       for new_player1 in new_player1s]
         else:
-            new_player2s = [((player2 + score - 1) % 10) + 1 for score in scores]
-            s = [self.play(player1, new_player2, player1_score, player2_score + new_player2, True) for new_player2 in new_player2s]
-            return reduce(lambda x, y: x+y, s)
+            new_player2s = [to_space(player2 + roll) for roll in dice_rolls]
+            results = [self.number_of_wins(player1, new_player2, player1_score, player2_score + new_player2, True)
+                       for new_player2 in new_player2s]
 
-
-    # def play(self):
-    #     while self.player1_score < 1000 and self.player2_score < 1000:
-    #         self.play_next()
-    #     return self.round * min(self.player1_score, self.player2_score)
-    #
-    # def play_next(self):
-    #     print(score)
-    #     if self.round % 2 == 1:
-    #         self.player1 = ((self.player1 + score - 1) % 10) + 1
-    #         self.player1_score += self.player1
-    #     else:
-    #         self.player2 = ((self.player2 + score - 1) % 10) + 1
-    #         self.player2_score += self.player2
-
-    # def roll_dice(self):
-    #     self.round += 1
-    #     return ((self.round - 1) % 100) + 1
+        return reduce(add, results)
 
 
 def main():
@@ -102,8 +87,7 @@ def main():
     game1 = Game1(player1, player2)
     print("part 1", game1.play())
     game2 = Game2(player1, player2)
-    print("part 2", game2.play_from_start())
+    print("part 2", game2.play())
 
 
-print((1, 0) + (1, 1))
 main()
